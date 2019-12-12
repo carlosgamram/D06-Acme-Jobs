2dcba212f113e64b422c1de9fb68de771e228410
Jose Manuel <josvilgar1@alum.us.es>
2019-12-12 17:40:35 +0100

    Task 009 started

---
+++ src/main/java/acme/features/authenticated/message/AuthenticatedMessageCreateService.java
@@ -0,0 +1,77 @@
+
+package acme.features.authenticated.message;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.messages.Message;
+import acme.entities.messagethreads.Messagethread;
+import acme.framework.components.Errors;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Authenticated;
+import acme.framework.entities.Principal;
+import acme.framework.services.AbstractCreateService;
+
+@Service
+public class AuthenticatedMessageCreateService implements AbstractCreateService<Authenticated, Message> {
+
+	@Autowired
+	AuthenticatedMessageRepository repository;
+
+
+	@Override
+	public boolean authorise(final Request<Message> request) {
+		assert request != null;
+		// TODO:
+		//1: Comprobar que el usuario que está queriendo
+		//crear un nuevo mensaje en un hilo, es realmente un usuario de ese hilo
+		return true;
+	}
+
+	@Override
+	public void bind(final Request<Message> request, final Message entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		request.bind(entity, errors, "confirmation");
+	}
+
+	@Override
+	public void unbind(final Request<Message> request, final Message entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "creationMoment", "title", "tags", "body");
+	}
+
+	@Override
+	public Message instantiate(final Request<Message> request) {
+		assert request != null;
+
+		Principal principal = request.getPrincipal();
+		Authenticated authenticated = this.repository.findAuthenticatedById(principal.getActiveRoleId());
+
+		Messagethread messageThread = this.repository.findMessageThreadById(request.getModel().getInteger("id"));
+
+		Message result = new Message();
+
+		result.setAuthenticated(authenticated);
+		result.setMessageThread(messageThread);
+		return result;
+	}
+
+	@Override
+	public void validate(final Request<Message> request, final Message entity, final Errors errors) {
+
+	}
+
+	@Override
+	public void create(final Request<Message> request, final Message entity) {
+		this.repository.save(entity);
+
+	}
+
+}
--- src/main/java/acme/features/authenticated/message/AuthenticatedMessageRepository.java
+++ src/main/java/acme/features/authenticated/message/AuthenticatedMessageRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.entities.messages.Message;
+import acme.entities.messagethreads.Messagethread;
+import acme.framework.entities.Authenticated;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
@@ -15,7 +17,12 @@ public interface AuthenticatedMessageRepository extends AbstractRepository {
 	@Query("select a from Message a where a.id = ?1")
 	Message findOneById(int id);
 
-	@Query("select mt from Message mt where mt.messageThread.id = ?1")
+	@Query("select m from Message m where mm.messageThread.id = ?1")
 	Collection<Message> findManyByMessagethread(int id);
 
+	@Query("select mt from MessageThread mt where mt.id = ?1")
+	Messagethread findMessageThreadById(int messageThreadid);
+
+	@Query("select a from Authenticated a where a.id = ?1")
+	Authenticated findAuthenticatedById(int authenticatedId);
 }
