f05a387e9cabbe2e8e8b14c1c315a8204c1afdeb
Jose Manuel <josvilgar1@alum.us.es>
2019-12-18 20:09:22 +0100

    penultimos commits bugs

--- src/main/java/acme/features/auditor/auditorrecord/AuditorAuditrecordUpdateService.java
+++ src/main/java/acme/features/auditor/auditorrecord/AuditorAuditrecordUpdateService.java
@@ -1,28 +1,23 @@
 
 package acme.features.auditor.auditorrecord;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.auditrecords.Auditrecord;
 import acme.entities.configuration.SpamUtils;
-import acme.entities.jobs.Job;
 import acme.entities.roles.Auditor;
 import acme.framework.components.Errors;
+import acme.framework.components.HttpMethod;
 import acme.framework.components.Model;
 import acme.framework.components.Request;
+import acme.framework.components.Response;
 import acme.framework.entities.Principal;
-import acme.framework.services.AbstractCreateService;
-
-// Apuntes:
-// -Comprobar que los auditrecords que no estan en modo final no se pueden listar.
-// -Repasar el authorise
-// -A el create de esta feature se accede desde el job
+import acme.framework.helpers.PrincipalHelper;
+import acme.framework.services.AbstractUpdateService;
 
 @Service
-public class AuditorAuditrecordCreateService implements AbstractCreateService<Auditor, Auditrecord> {
+public class AuditorAuditrecordUpdateService implements AbstractUpdateService<Auditor, Auditrecord> {
 
 	@Autowired
 	AuditorAuditrecordRepository	repository;
@@ -35,11 +30,13 @@ public class AuditorAuditrecordCreateService implements AbstractCreateService<Au
 	public boolean authorise(final Request<Auditrecord> request) {
 		assert request != null;
 
-		Boolean result;
+		Boolean result = true;
 
 		Principal principal = request.getPrincipal();
+		int auditRecId = request.getModel().getInteger("id");
+		Auditrecord auditRecord = this.repository.findOneById(auditRecId);
 
-		result = principal.getActiveRole() == Auditor.class;
+		result = principal.getActiveRoleId() == auditRecord.getAuditor().getId();
 
 		return result;
 	}
@@ -50,7 +47,7 @@ public class AuditorAuditrecordCreateService implements AbstractCreateService<Au
 		assert entity != null;
 		assert errors != null;
 
-		request.bind(entity, errors, "creationMoment");
+		request.bind(entity, errors);
 	}
 
 	@Override
@@ -59,28 +56,17 @@ public class AuditorAuditrecordCreateService implements AbstractCreateService<Au
 		assert entity != null;
 		assert model != null;
 
-		request.unbind(entity, model, "title", "finalMode", "body", "job.id");
+		request.unbind(entity, model, "creationMoment", "title", "finalMode", "body");
 	}
 
 	@Override
-	public Auditrecord instantiate(final Request<Auditrecord> request) {
+	public Auditrecord findOne(final Request<Auditrecord> request) {
 		assert request != null;
 
-		Integer id = request.getModel().getInteger("job.id");
-		if (id == null) {
-			id = request.getModel().getInteger("id");
-		}
+		Auditrecord result;
 
-		int roleId = request.getPrincipal().getActiveRoleId();
-
-		Job job = this.repository.findJobById(id);
-		Auditor auditor = this.repository.findAuditorById(roleId);
-
-		Auditrecord result = new Auditrecord();
-
-		result.setJob(job);
-		result.setAuditor(auditor);
-		result.setFinalMode(false);
+		int id = request.getModel().getInteger("id");
+		result = this.repository.findOneById(id);
 
 		return result;
 	}
@@ -94,20 +80,24 @@ public class AuditorAuditrecordCreateService implements AbstractCreateService<Au
 		String body = entity.getBody();
 
 		errors.state(request, !this.spamUtils.checkSpam(body), "body", "auditor.auditrecord.form.errors.spamControl");
-
 	}
 
 	@Override
-	public void create(final Request<Auditrecord> request, final Auditrecord entity) {
+	public void update(final Request<Auditrecord> request, final Auditrecord entity) {
 		assert request != null;
 		assert entity != null;
 
-		Date moment;
-
-		moment = new Date(System.currentTimeMillis() - 1);
-		entity.setCreationMoment(moment);
-
 		this.repository.save(entity);
+	}
+
+	@Override
+	public void onSuccess(final Request<Auditrecord> request, final Response<Auditrecord> response) {
+		assert request != null;
+		assert response != null;
+
+		if (request.isMethod(HttpMethod.POST)) {
+			PrincipalHelper.handleUpdate();
+		}
 	}
 
 }
