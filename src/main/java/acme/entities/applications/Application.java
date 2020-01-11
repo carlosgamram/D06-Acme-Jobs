a8b184ab6dd17215927210143ed7cd71e511b7e8
cargamram <cargamram@alum.us.es>
2020-01-11 20:35:11 +0100

    deleted exam files

--- src/main/java/acme/entities/applications/Application.java
+++ src/main/java/acme/entities/applications/Application.java
@@ -60,16 +60,6 @@ public class Application extends DomainEntity {
 
 	private String				justification;
 
-	//TODO: Cambiar
-	private String				XXXRequestResponse;
-
-	//TODO: Cambiar
-	private String				xxx;
-
-	//TODO: Cambiar
-	//	@Pattern(regexp = "([A-Z])\\w+")
-	private String				password;
-
 	//Relationships ---------------------------------------
 
 	@NotNull
